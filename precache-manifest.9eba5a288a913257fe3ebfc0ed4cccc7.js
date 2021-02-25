e958b00e8855a8edff58add455b41a4e0f057d4c
Rubin78 <rubinshaikh71@gmail.com>
2021-02-25 17:47:33 +0530

    Updates

---
+++ precache-manifest.9eba5a288a913257fe3ebfc0ed4cccc7.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb600772e73da196e51ca1cff69bdb0",
+    "url": "/AminSiddiqui.com/index.html"
+  },
+  {
+    "revision": "510c843a384557617686",
+    "url": "/AminSiddiqui.com/static/css/2.500cb03f.chunk.css"
+  },
+  {
+    "revision": "a5a779c5c23c5a291835",
+    "url": "/AminSiddiqui.com/static/css/main.9d708822.chunk.css"
+  },
+  {
+    "revision": "510c843a384557617686",
+    "url": "/AminSiddiqui.com/static/js/2.02976b85.chunk.js"
+  },
+  {
+    "revision": "b45be4dfcf889ea1b7385f58d15dba2d",
+    "url": "/AminSiddiqui.com/static/js/2.02976b85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5a779c5c23c5a291835",
+    "url": "/AminSiddiqui.com/static/js/main.c73a7790.chunk.js"
+  },
+  {
+    "revision": "5c730eebaf0c9b8b0f20",
+    "url": "/AminSiddiqui.com/static/js/runtime-main.e083756b.js"
+  },
+  {
+    "revision": "1635b5d09d8729b6b1f80f2e9e7f3c4e",
+    "url": "/AminSiddiqui.com/static/media/Modelling1.1635b5d0.png"
+  },
+  {
+    "revision": "6326b1b8789bd06ce15a39d3c9f7aa95",
+    "url": "/AminSiddiqui.com/static/media/amin4.6326b1b8.png"
+  },
+  {
+    "revision": "6eb5ec380eda22c4ddb9cfb91ac17f4a",
+    "url": "/AminSiddiqui.com/static/media/five.6eb5ec38.png"
+  },
+  {
+    "revision": "522e1210ff6554d4f8d0f3adf324916c",
+    "url": "/AminSiddiqui.com/static/media/four.522e1210.png"
+  },
+  {
+    "revision": "8354c47c324cddf656b7e584255f21b4",
+    "url": "/AminSiddiqui.com/static/media/nine.8354c47c.png"
+  },
+  {
+    "revision": "04e39e993b1a96d99f2d68a0a5822417",
+    "url": "/AminSiddiqui.com/static/media/port3.04e39e99.jpg"
+  },
+  {
+    "revision": "b8f14452ff8202102c00df8c1319b8e7",
+    "url": "/AminSiddiqui.com/static/media/seven.b8f14452.png"
+  },
+  {
+    "revision": "71b134b4f3aca648e545dbdd777b6870",
+    "url": "/AminSiddiqui.com/static/media/six.71b134b4.png"
+  },
+  {
+    "revision": "b0d7c5ab278538d875f0e310211d2e83",
+    "url": "/AminSiddiqui.com/static/media/ten.b0d7c5ab.png"
+  },
+  {
+    "revision": "b313f9ca957b592e3ff9d5be6b7436a1",
+    "url": "/AminSiddiqui.com/static/media/three.b313f9ca.png"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/AminSiddiqui.com/static/media/tosee2.e95d0563.webp"
+  }
+]);
